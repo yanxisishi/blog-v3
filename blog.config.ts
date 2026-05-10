394d289fedd7569cc56577190602aea98e4cf50e
yanxisishi <3497863696@qq.com>
2026-05-10 13:06:43 +0800

    博客更新ing

--- blog.config.ts
+++ blog.config.ts
@@ -84,8 +84,8 @@ const blogConfig = {
 
 	/** 自己部署的 Twikoo 服务 */
 	twikoo: {
-		envId: 'https://twikoo.example.site/',
-		preload: 'https://twikoo.example.site/',
+		envId: 'https://twikoo.yanxisishi.top',
+		preload: 'https://twikoo.yanxisishi.top',
 	},
 }
 
